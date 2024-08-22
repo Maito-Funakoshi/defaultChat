00999f932cd5d0bd72f866a4b76dc4295e2b101a
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-08-22 14:41:04 +0900

    logit_bias: {95839:100}

--- src/AssistantResponses.jsx
+++ src/AssistantResponses.jsx
@@ -21,7 +21,7 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, characters
                   ...currentMessages.map(message => ({...message, role: "user"}))
               ];
               let response = await openai.chat.completions.create({
-                logit_bias: {15121:-100, 14429:-100},
+                logit_bias: {95839:100},
                 model: "gpt-4o",
                 messages: chatMessages
               })
