a1d14f7f87cb87fb0170d216b858b9024f0f2fda
Maito-Funakoshi <funakoshi-maito728@g.ecc.u-tokyo.ac.jp>
2024-09-14 13:00:20 +0900

    OD以外の要素を提案アプリに統一、slice削除

--- src/AssistantResponses.jsx
+++ src/AssistantResponses.jsx
@@ -9,20 +9,29 @@ const AssistantResponses = ({ names, namesEng, messages, setMessages, characters
     dangerouslyAllowBrowser: true
   })
 
-  const maxContextMessages = 100;
-
   useEffect(() => {
     if (messages.length > 2　&& messages[messages.length - 1].role == "user") {
       const makeResponse = async () => {
-        let currentMessages = [...messages].slice(-maxContextMessages);
             try {
               const chatMessages = [
                 { role: "system", content: `あなたは${names[0]}という名前のアシスタントです。以下の設定をもとに返答を作成してください。${chat} あなたの特徴は以下の通りです。${characters[0]}` },
-                  ...currentMessages.map(message => ({...message, role: "user"}))
+                  ...messages.map(message => ({...message, role: "user"}))
               ];
               let response = await openai.chat.completions.create({
                 model: "gpt-4o",
-                messages: chatMessages
+                messages: chatMessages,
+                temperature: 1.2
+              })
+
+                // 発言様式を整備する
+              const commonMessages = [
+                  { role: "system", content: `${common}`},
+                  { role: "user", content: `${response.choices[0].message.content.trim()}`}
+              ];
+              response = await openai.chat.completions.create({
+                model: "gpt-4o",
+                messages: commonMessages,
+                temperature: 1.2
               })
 
               if (response.choices && response.choices.length > 0) {
